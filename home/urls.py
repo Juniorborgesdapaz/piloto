83118f6e9bdcdc9a46e93549cbdab815a76d6a8d
Juniorborgesdapaz <139707140+Juniorborgesdapaz@users.noreply.github.com>
2024-11-07 13:24:15 -0300

    subindo

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
-    path('', views.index, name="index"),
+urlpatterns = [
+    #path('', views.index, name='index'),
+    path('sobre/', views.sobre, name='sobre'),
+    
+      # Associa a URL raiz do app com a view `index`
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def index(request):
-    return HttpResponse("a view etsa funcionado")
+def sobre(request):
+    return render(request, 'sobre.html')
+
